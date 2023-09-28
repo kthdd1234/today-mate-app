ca7515c8912f9370f5d5e701d8627939b46922ec
kthdd1234 <kthdd12@daum.net>
2023-09-28 20:54:15 +0900

    feat: today-mate-app 초기 환경 세팅 완료

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <View>
+      <Text>12342</Text>
+    </View>
+  );
+}
+
+export default App;
